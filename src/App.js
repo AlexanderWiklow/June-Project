a8c38ba35eb75237cf907eb258888f1dc6a63816
AlexanderWiklow <a.wiklow@gmail.com>
2023-07-03 14:23:09 +0200

    deploy website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  HashRouter,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import BlogPost from './pages/BlogPost';
 
 const App = () => {
   return (
-    <HashRouter basename="/">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog/:id" element={<BlogPost />} />
-        </Routes>
-      </Router>
-    </HashRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </Router>
   );
 };
 
